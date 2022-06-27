a4100bda5ba81a21435f05c4841252c4866c5a73
Bhagyashree_nm_ <106364694+Bhagyashree2k2@users.noreply.github.com>
2022-06-27 21:43:19 +0530

    Update bipartite_max.cpp

--- bipartite_max.cpp
+++ bipartite_max.cpp
@@ -2,16 +2,15 @@
 #define M 6
 #define N 6
 using namespace std;
-
+int setA[M], setB[N];
 bool bipartiteMatch(bool bipartiteGraph[M][N],int u, bool visited[], int assign[]) {
    for (int v = 0; v < N; v++) 
-   {    //for all jobs 0 to N-1
+   {    
       if (bipartiteGraph[u][v] && !visited[v])
-      {    //when job v is not visited and u is interested
-         visited[v] = true;    //mark as job v is visited
-         //when v is not assigned or previously assigned
+      {    
+         visited[v] = true;    
          if (assign[v] < 0 || bipartiteMatch(bipartiteGraph,assign[v], visited, assign)) {
-            assign[v] = u;    //assign job v to applicant u
+            assign[v] = u;    
             return true;
          }
       }
@@ -21,34 +20,41 @@ bool bipartiteMatch(bool bipartiteGraph[M][N],int u, bool visited[], int assign[
 
 int maxMatch(bool bipartiteGraph[M][N])
 {
-   int assign[N];    //an array to track which job is assigned to which applicant
+   int assign[N];    
    for(int i = 0; i<N; i++)
-      assign[i] = -1;    //initially set all jobs are available
-   int jobCount = 0;
-
+      assign[i] = -1;    
+   int result = 0;
+    bool visited[N];
    for (int u = 0; u < M; u++)
-   {    //for all applicants
-      bool visited[N];
+   {    
+      
       for(int i = 0; i<N; i++)
-         visited[i] = false;    //initially no jobs are visited
-      if (bipartiteMatch(bipartiteGraph,u, visited, assign))    //when u get a job
-         jobCount++;
+         visited[i] = false;    
+      if (bipartiteMatch(bipartiteGraph,u, visited, assign))    
+         result++;
    }
-   return jobCount;
+   cout<<"Matched vertices set = {";
+     for(int i=0; i<N; i++){
+        cout<<"("<<setA[i]<<", "<<setB[visited[i]]<<")";
+        if(i != M-1) cout<<", ";
+     }
+     cout<<"}";
+     cout<<endl;
+   return result;
 }
 
 int main() 
 {
     int m,n;
-    cout<<"Enter the no of applicants:\n";
+    cout<<"Enter number of vertices in set A\n";
     cin>>m;
-    cout<<"Enter the no of jobs:\n";
+    cout<<"Enter number of vertices in set B\n";
     cin>>n;
     bool bipartiteGraph[M][N];
-    cout<<"Enter which job has been applied by which applicant:\n";
+    cout<<"Enter the adjacency matrix describing the relation between the two set of vertices:\n";
     for(int i=0;i<M;i++)
      for(int j=0;j<N;j++)
         cin>>bipartiteGraph[i][j];
-   cout << "Maximum number of applicants matching for job: " << maxMatch(bipartiteGraph);
+   cout << "Maximum number of matching \n: " << maxMatch(bipartiteGraph);
    return 0;
 }
